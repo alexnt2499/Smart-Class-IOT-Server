0bb41072d2c104e121f4938db6b87ab5dc4cfa1c
ntd2471999 <nguyenthanhduy247199@gmail.com>
2019-07-31 13:34:32 +0700

    make it better

--- routers/index.js
+++ routers/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req,res) => {
-    res.send('Welcome server Duy Đẹp Trai ');
     client.emit('hello','ăn đấm không?');
+    res.send('Welcome server Duy Đẹp Trai ');
+    
 })
 
 module.exports = router;
