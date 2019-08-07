32e06f4cd9b8622eb0d5b5dbb149bc4810b50af1
ntd2471999 <nguyenthanhduy247199@gmail.com>
2019-08-08 00:21:36 +0700

    better

--- model/Room.js
+++ model/Room.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 var schema = new mongoose.Schema({
     nameRoom : {
-        type : 'number',
+        type : 'string',
         required : true,
         unique : true
     },
